7b07c8286d98da223669cb3409861695725b5fd8
Pranav Sindura <pranav.sindura@gmail.com>
2021-04-14 17:32:42 +0530

    fix 3d issue

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import stego
 import cv2
+import numpy as np
 from PIL import Image
 # images are stored in src/
 # Retrieve host image host_img
 # Retrieve secret image secret_img
-host_img = cv2.imread('src/lena.png')
-secret_img = cv2.imread('src/baboon.png')
+
+host_img = np.array(Image.open('src/lena.png'))
+secret_img = np.array(Image.open('src/baboon.png'))
 
 # Encrypt
 stego_img, key = stego.encrypt(host_img, secret_img)
-im = Image.fromarray(stego_img)
+im = Image.fromarray(host_img)
 im.show()
-im.save("result.png")
+im.save("result.jpg")
 # Save the stego_img and key
 
 # Decrypt
